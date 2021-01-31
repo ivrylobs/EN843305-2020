ed6060727046cca284f273a4e1dbec80eb0de4c6
Isaac Khun <ivrylobs>
2021-01-31 21:19:43 +0700

    Add controller script

---
+++ Labs/src/fruit eater 613030651-2/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public bool isLeft;
+    public book isRight;
+    public book isJump;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyboardControl();
+
+        if (isLeft)
+        {
+            rb.AddForce(Vector2.left * 10);
+        } else if (isLeft)
+        {
+            rb.AddForce(Vector2.right * 10);
+        }
+
+        if (isJump)
+        {
+            rb.AddForce(Vector2.up * 10);
+            isJump = false;
+        }
+    }
+
+    void KeyboardControl()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            SetLeft(true);
+        }
+
+        if (Input.GetKeyUp("a"))
+        {
+            SetLeft(false);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            SetRight(true);
+        }
+
+        if (Input.GetKeyUp("d"))
+        {
+            SetRight(false);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            SetJump(true);
+        }
+    }
+
+    public void SetLeft(bool b)
+    {
+        isLeft = b;
+    }
+
+    public void SetRight(bool b)
+    {
+        isRight = b;
+    }
+
+    public void SetJump(bool b)
+    {
+        isJump = b;
+    }
+}
